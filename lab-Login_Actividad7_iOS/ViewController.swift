b2d993077b03b4e06211cc1bdc08ca54665569a4
AngelMezaSchool <al03018750@tecmilenio.mx>
2024-03-15 22:08:07 -0600

    Initial Commit

---
+++ lab-Login_Actividad7_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab-Login_Actividad7_iOS
+//
+//  Created by Angel Meza on 15/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
