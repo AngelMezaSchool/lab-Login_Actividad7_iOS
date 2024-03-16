782e8faa2e88094416365fe318e8814bf4f1fed5
AngelMezaSchool <al03018750@tecmilenio.mx>
2024-03-15 23:42:37 -0600

    TERMIANDO

--- lab-Login_Actividad7_iOS/ViewController.swift
+++ lab-Login_Actividad7_iOS/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var forgotUser: UIButton!
+    
+    @IBOutlet weak var forgotPassword: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    // s
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //segue.destination.navigationItem.title = userName.text
+        guard let sender = sender as? UIButton else { return }
+        if sender == forgotUser {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else if sender == forgotPassword{
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else {
+            segue.destination.navigationItem.title = userName.text
+        }
+    }
+    
+    
+    @IBAction func forgotUser2(_ sender: UIButton) {
+        performSegue(withIdentifier: "forgot", sender: forgotUser)
+    }
+    
+    @IBAction func forgotPassword2(_ sender: UIButton) {
+        performSegue(withIdentifier: "forgot", sender: forgotPassword)
+    }
+    
 }
 
